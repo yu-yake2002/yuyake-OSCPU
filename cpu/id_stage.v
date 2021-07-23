d0e6e172555da21ea46459e335bb0be4341ae919
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-24 07:54:36 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: 5dc5e9891fc64cb097905b436c167a11     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  07:54:37 up 10:15,  1 user,  load average: 0.59, 0.23, 0.18

--- cpu/id_stage.v
+++ cpu/id_stage.v
@@ -201,7 +201,7 @@ assign rd_w_ena   = ~rst & (inst_i_load | inst_i_fence | inst_i_arith_dword
                           | inst_u_lui | inst_r_word | inst_i_jalr | inst_j
                           | inst_i_sys);
 assign rd_w_addr  = (rd_w_ena == 1'b1) ? rd : 0;
-*/
+
 assign exe_op1 = {64{~rst}} & (
                 ({64{inst_i_load}}        & r_data1)
               | ({64{inst_i_fence}}       & r_data1)
@@ -238,7 +238,7 @@ wire inst_load = ~opcode[2] & ~opcode[3] & ~opcode[4] & ~opcode[5] & ~opcode[6];
 assign mem_to_reg = (rst == 1'b1) ? 0 : inst_load;
 wire inst_save = ~opcode[2] & ~opcode[3] & ~opcode[4] & opcode[5] & ~opcode[6];
 assign mem_w_ena = (rst == 1'b1) ? 0 : inst_save;
-
+*/
 
 
 wire inst_addi =   ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[6]
@@ -264,5 +264,14 @@ assign rs2_r_addr = 0;
 
 assign rd_w_ena   = ( rst == 1'b1 ) ? 0 : inst_type[4];
 assign rd_w_addr  = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? rd  : 0 );
+
+assign exe_op1 = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? r_data1 : 0 );
+assign exe_op2 = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? { {52{immI[11]}}, immI } : 0 );
+
+wire inst_load = ~opcode[2] & ~opcode[3] & ~opcode[4] & ~opcode[5] & ~opcode[6];
+assign mem_to_reg = (rst == 1'b1) ? 0 : inst_load;
+wire inst_save = ~opcode[2] & ~opcode[3] & ~opcode[4] & opcode[5] & ~opcode[6];
+assign mem_w_ena = (rst == 1'b1) ? 0 : inst_save;
+
 
 endmodule
