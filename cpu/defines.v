b92a259b504e0f663b90e06a19d7965967ede104
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-23 23:18:04 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: 5dc5e9891fc64cb097905b436c167a11     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  23:18:04 up  1:38,  1 user,  load average: 0.49, 0.18, 0.11

--- cpu/defines.v
+++ cpu/defines.v
@@ -4,3 +4,40 @@
 `define ZERO_WORD  64'h00000000_00000000   
 `define REG_BUS    63 : 0     
 `define INST_ADD   8'h11
+
+`define ALU_ADD    0
+`define ALU_SUB    1
+`define ALU_SLT    2
+`define ALU_SLTU   3
+`define ALU_XOR    4
+`define ALU_OR     5
+`define ALU_AND    6
+`define ALU_SLL    7
+`define ALU_SRL    8
+`define ALU_SRA    9
+`define ALU_BUS    9 : 0
+
+`define BJ_BEQ  0
+`define BJ_BNE  1
+`define BJ_BLT  2
+`define BJ_BGE  3
+`define BJ_BLTU 4
+`define BJ_BGEU 5
+`define BJ_JALR 6
+`define BJ_JAL  7
+`define BJ_BUS  7 : 0
+
+`define INST_I_LOAD        0
+`define INST_I_FENCE       1
+`define INST_I_ARITH_DWORD 2
+`define INST_U_AUIPC       3
+`define INST_I_ARITH_WORD  4
+`define INST_S             5
+`define INST_R_DWORD       6
+`define INST_U_LUI         7
+`define INST_R_WORD        8
+`define INST_B             9
+`define INST_I_JALR        10
+`define INST_J             11
+`define INST_I_SYS         12
+`define OP_BUS             12 : 0
