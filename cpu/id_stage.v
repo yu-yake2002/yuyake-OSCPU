86daf27cc6ae3e6e3dd255ed4034a45592a5ccfc
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-24 07:33:04 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: 5dc5e9891fc64cb097905b436c167a11     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  07:33:04 up  9:53,  1 user,  load average: 0.65, 0.38, 0.24

--- cpu/id_stage.v
+++ cpu/id_stage.v
@@ -193,8 +193,8 @@ assign rs1_r_ena  = ~rst & (inst_i_load | inst_i_fence | inst_i_arith_dword
                           | inst_i_arith_word | inst_r_dword | inst_r_word
                           | inst_b | inst_i_jalr | inst_i_sys);
 assign rs1_r_addr = (rs1_r_ena == 1'b1) ? rs1 : 0;
-assign rs2_r_ena  = ~rst & (inst_r_dword | inst_r_word | inst_s | inst_b);
-assign rs2_r_addr = (rs2_r_ena == 1'b1) ? rs2 : 0;
+//assign rs2_r_ena  = ~rst & (inst_r_dword | inst_r_word | inst_s | inst_b);
+//assign rs2_r_addr = (rs2_r_ena == 1'b1) ? rs2 : 0;
 /*
 assign rd_w_ena   = ~rst & (inst_i_load | inst_i_fence | inst_i_arith_dword
                           | inst_u_auipc | inst_i_arith_word | inst_r_dword
@@ -258,9 +258,10 @@ assign inst_opcode[7] = (  rst == 1'b1 ) ? 0 : 0;
 /*
 assign rs1_r_ena  = ( rst == 1'b1 ) ? 0 : inst_type[4];
 assign rs1_r_addr = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? rs1 : 0 );
+*/
 assign rs2_r_ena  = 0;
 assign rs2_r_addr = 0;
-*/
+
 assign rd_w_ena   = ( rst == 1'b1 ) ? 0 : inst_type[4];
 assign rd_w_addr  = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? rd  : 0 );
 
