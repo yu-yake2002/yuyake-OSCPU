591270be2ac827b44feffed019f5999e5d680e92
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-24 08:25:35 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: 5dc5e9891fc64cb097905b436c167a11     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  08:25:35 up 10:46,  1 user,  load average: 1.00, 0.44, 0.23

--- cpu/id_stage.v
+++ cpu/id_stage.v
@@ -234,20 +234,6 @@ assign jmp_imm = ({64{inst_b}} & {{51{immB[12]}}, immB})
                | ({64{inst_j}} & {{43{immJ[20]}}, immJ})
                | ({64{inst_i_jalr}} & r_data1 + {{52{immI[11]}}, immI} - inst_addr);
 
-
-
-// I-type
-wire [6  : 0]opcode;
-wire [4  : 0]rd;
-wire [2  : 0]func3;
-wire [4  : 0]rs1;
-wire [11 : 0]imm;
-assign opcode = inst[6  :  0];
-assign rd     = inst[11 :  7];
-assign func3  = inst[14 : 12];
-assign rs1    = inst[19 : 15];
-assign imm    = inst[31 : 20];
-
 wire inst_addi =   ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[6]
                  & ~func3[0] & ~func3[1] & ~func3[2];
 
@@ -263,9 +249,6 @@ assign inst_opcode[4] = (  rst == 1'b1 ) ? 0 : inst_addi;
 assign inst_opcode[5] = (  rst == 1'b1 ) ? 0 : 0;
 assign inst_opcode[6] = (  rst == 1'b1 ) ? 0 : 0;
 assign inst_opcode[7] = (  rst == 1'b1 ) ? 0 : 0;
-
-
-
 
 assign rs1_r_ena  = ( rst == 1'b1 ) ? 0 : inst_type[4];
 assign rs1_r_addr = ( rst == 1'b1 ) ? 0 : ( inst_type[4] == 1'b1 ? rs1 : 0 );
