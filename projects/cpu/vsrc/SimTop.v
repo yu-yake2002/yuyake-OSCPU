7e36f856fe8e5e1b7b6fe604c44fea16ccd01353
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-08 22:49:01 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: ad05bda3d2b545aa8bfaefe9be6c639b     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  22:49:01 up 3 days,  1:39,  1 user,  load average: 0.34, 0.29, 0.20

--- projects/cpu/vsrc/SimTop.v
+++ projects/cpu/vsrc/SimTop.v
@@ -21,303 +21,297 @@ module SimTop(
   output [7:0]  io_uart_out_ch,
   output        io_uart_in_valid,
   input  [7:0]  io_uart_in_ch
-);
+  );
 
+  // Access memory
+  // mem_stage <-> mem_helper
+  wire [`REG_BUS] mem_addr = exe_data;
+  wire [7 : 0] byte_enable; // write mask
+  wire [`REG_BUS] mem_wr_data;
+  wire [`REG_BUS] mem_rd_data;
+  RAM_1W2R RAM_1W2R(
+    .clk(clock),
+    
+    .inst_addr(pc),
+    .inst_ena(1),
+    .inst(inst),
+    
+    .mem_wr_ena(mem_wr_ena),
+    .mem_rd_ena(mem_rd_ena),
+    .byte_enable(byte_enable),
+    .mem_addr(mem_addr),
+    .mem_wr_data(mem_wr_data),
+    .mem_rd_data(mem_rd_data)
+  );
 
-// if_stage
-wire [31 : 0]  inst;
-wire [63 : 0]  pc; 
-wire           inst_ena;
-// if -> exception
-wire [`EXCP_BUS] if_excp;
-
-// id_stage
-// id_stage -> regfile
-wire rs1_r_ena;
-wire [4 : 0]rs1_r_addr;
-wire rs2_r_ena;
-wire [4 : 0]rs2_r_addr;
-wire rd_w_ena;
-wire [4 : 0]rd_w_addr;
-// id_stage -> csrfile
-wire csr_rd_ena;
-wire [11 : 0] csr_rd_addr;
-wire csr_wr_ena;
-wire [11 : 0] csr_wr_addr;
-
-// id_stage -> exe/mem_stage
-wire [`REG_BUS] exe_op1;
-wire [`REG_BUS] exe_op2;
-wire [`OP_BUS] op_info;
-wire [`ALU_BUS] alu_info;
-wire [`BJ_BUS]  bj_info;
-wire [`LOAD_BUS] load_info;
-wire [`SAVE_BUS] save_info;
-wire is_word_opt;
-
-// id -> exception
-wire [`EXCP_BUS] id_excp;
-
-// id stage -> mem_stage
-wire mem_wr_ena;
-wire mem_rd_ena;
-// id stage -> wb stage
-wire [`REG_CTRL_BUS] reg_wr_ctrl;
-
-// id_stage -> ex_stage
-wire [`REG_BUS] jmp_imm;
-
-// exception
-wire excp_enter, excp_exit;
-// exception -> if_stage
-wire excp_jmp_ena;
-wire [`REG_BUS] excp_pc;
-// exception -> CSRs
-wire [`REG_BUS] mstatus_wr_data;
-wire [`REG_BUS] mstatus_rd_data;
-wire [`REG_BUS] mie_rd_data;
-wire [`REG_BUS] mtvec_rd_data;
-wire [`REG_BUS] mscratch_rd_data;
-wire [`REG_BUS] mepc_wr_data;
-wire [`REG_BUS] mepc_rd_data;
-wire [`REG_BUS] mcause_wr_data;
-wire [`REG_BUS] mcause_rd_data;
-wire [`REG_BUS] mtval_rd_data;
-wire [`REG_BUS] mtval_wr_data;
-wire [`REG_BUS] mip_rd_data;
-
-// regfile -> id_stage
-wire [`REG_BUS] r_data1;
-wire [`REG_BUS] r_data2;
-// regfile -> difftest
-wire [`REG_BUS] regs[0 : 31];
-
-// csrfile -> id/wb_stage
-wire [`REG_BUS] csr_rd_data;
-// csrfile -> difftest
-wire [`REG_BUS] csr_o[`CSR_BUS];
-
-// exe_stage -> mem/wb_stage
-wire [`REG_BUS] exe_data;
-wire [`REG_BUS] mem_addr = exe_data;
-wire [`REG_BUS] csr_wr_data;
-// exe_stage -> if_stage
-wire bj_ena;
-wire [`REG_BUS] new_pc;
-
-// mem_helper -> mem_stage
-wire [`REG_BUS] mem_rd_data;
-
-// mem_stage -> wb_stage
-wire [`REG_BUS] mem_data;
-// mem_stage -> mem_helper
-wire [7 : 0] byte_enable;
-wire [`REG_BUS] mem_wr_data;
-// mem_stage -> excp_handler
-wire [`EXCP_BUS] mem_excp;
-
-// wb_stage -> regfile
-wire [`REG_BUS] rd_data;
-
-// Access memory
-RAM_1W2R RAM_1W2R(
-  .clk(clock),
+  // IF_STAGE
+  wire [31 : 0]  inst;
+  wire [63 : 0]  pc; 
+  wire           inst_ena;
+  // if -> exception
+  wire [`EXCP_BUS] if_excp;
+  // pipeline control
+  wire if_ready_go;
+  wire if_id_valid;
+  // instruction jump & exception jump
+  wire bj_ena;
+  wire [`REG_BUS] new_pc;
+  wire excp_jmp_ena;
+  wire [`REG_BUS] excp_pc;
   
-  .inst_addr(pc),
-  .inst_ena(1),
-  .inst(inst),
+  if_stage If_stage(
+    .clk(clock),
+    .rst(reset),
+    
+    .id_allowin(id_allowin),
+    .if_ready_go(if_ready_go),
+    .if_id_valid(if_id_valid),
+    
+    .bj_ena(bj_ena),
+    .new_pc(new_pc),
+    
+    .excp_jmp_ena(excp_jmp_ena),
+    .excp_pc(excp_pc),
+    
+    .pc(pc),
+    .if_excp(if_excp)
+  );
 
-  .mem_wr_ena(mem_wr_ena),
-  .mem_rd_ena(mem_rd_ena),
-  .byte_enable(byte_enable),
-  .mem_addr(mem_addr),
-  .mem_wr_data(mem_wr_data),
-  .mem_rd_data(mem_rd_data)
-);
-
-wire if_ready_go;
-wire if_id_valid;
-
-if_stage If_stage(
-  .clk(clock),
-  .rst(reset),
+  // ID_STAGE
+  // pipeline control
+  wire id_allowin;
+  wire id_ready_go;
+  wire id_ex_valid;
+  // decode result
+  wire [`OP_BUS] op_info;
+  wire [`ALU_BUS] alu_info;
+  wire [`BJ_BUS]  bj_info;
+  wire [`LOAD_BUS] load_info;
+  wire [`SAVE_BUS] save_info;
+  wire is_word_opt;
+  wire [`REG_BUS] jmp_imm;
+  // id -> exception
+  wire [`EXCP_BUS] id_excp;
+  // id stage -> wb stage
+  wire [`REG_CTRL_BUS] reg_wr_ctrl;
   
-  .id_allowin(id_allowin),
-  .if_ready_go(if_ready_go),
-  .if_id_valid(if_id_valid),
+  id_stage Id_stage(
+    .rst(reset),
+    .clk(clock),
+    
+    .ex_allowin(ex_allowin),
+    .id_allowin(id_allowin),
+    .id_ready_go(id_ready_go),
+    .id_ex_valid(id_ex_valid),
+    
+    .inst(inst),
+    .inst_addr(pc),
+    
+    .r_data1(r_data1),
+    .r_data2(r_data2),
+    .csr_data(csr_rd_data),
+    
+    .rs1_r_ena(rs1_r_ena),
+    .rs1_r_addr(rs1_r_addr),
+    .rs2_r_ena(rs2_r_ena),
+    .rs2_r_addr(rs2_r_addr),
+    .csr_rd_ena(csr_rd_ena),
+    .csr_rd_addr(csr_rd_addr),
+    
+    .mem_rd_ena(mem_rd_ena),
+    .mem_wr_ena(mem_wr_ena),
+    
+    .exe_op1(exe_op1),
+    .exe_op2(exe_op2),
+    .is_word_opt(is_word_opt),
+    .op_info(op_info),
+    .alu_info(alu_info),
+    .bj_info(bj_info),
+    .load_info(load_info),
+    .save_info(save_info),
+    
+    .reg_wr_ctrl(reg_wr_ctrl),
+    .csr_wr_ena(csr_wr_ena),
+    .csr_wr_addr(csr_wr_addr),
+    .rd_w_ena(rd_w_ena),
+    .rd_w_addr(rd_w_addr),
+    
+    .jmp_imm(jmp_imm),
+    
+    .id_excp(id_excp),
+    .excp_exit(excp_exit)
+  );
+    
+    
+  // EX_STAGE
+  // pipeline control
+  wire ex_allowin;
+  wire ex_ready_go;
+  wire ex_mem_valid;
+  // operand
+  wire [`REG_BUS] exe_op1;
+  wire [`REG_BUS] exe_op2;
 
-  .bj_ena(bj_ena),
-  .new_pc(new_pc),
+  ex_stage Ex_stage(
+    .clk(clock),
+    .rst(reset),
   
-  .excp_jmp_ena(excp_jmp_ena),
-  .excp_pc(excp_pc),
-
-  .pc(pc),
-  .if_excp(if_excp)
-);
-
-wire id_allowin;
-wire id_ready_go;
-wire id_ex_valid;
-
-id_stage Id_stage(
-  .rst(reset),
-  .clk(clock),
-
-  .ex_allowin(ex_allowin),
-  .id_allowin(id_allowin),
-  .id_ready_go(id_ready_go),
-  .id_ex_valid(id_ex_valid),
-
-  .inst(inst),
-  .inst_addr(pc),
-
-  .r_data1(r_data1),
-  .r_data2(r_data2),
-  .csr_data(csr_rd_data),
+    .mem_allowin(mem_allowin),
+    .ex_allowin(ex_allowin),
+    .ex_ready_go(ex_ready_go),
+    .ex_mem_valid(ex_mem_valid),
   
-  .rs1_r_ena(rs1_r_ena),
-  .rs1_r_addr(rs1_r_addr),
-  .rs2_r_ena(rs2_r_ena),
-  .rs2_r_addr(rs2_r_addr),
-  .csr_rd_ena(csr_rd_ena),
-  .csr_rd_addr(csr_rd_addr),
+    .exe_op1(exe_op1),
+    .exe_op2(exe_op2),
+    .is_word_opt(is_word_opt),
+    .op_info(op_info),
+    .alu_info(alu_info),
+    .bj_info(bj_info),
+    .jmp_imm(jmp_imm),
+    .now_pc(pc),
+
+    .rd_data(exe_data),
+    .new_pc(new_pc),
+    .bj_ena(bj_ena)
+  );
+
+  // MEM_STAGE
+  // pipeline control
+  wire mem_allowin;
+  wire mem_ready_go;
+  wire mem_wb_valid;
+  // mem_stage -> excp_handler
+  wire [`EXCP_BUS] mem_excp;
+  // mem control
+  wire mem_wr_ena;
+  wire mem_rd_ena;
   
-  .mem_rd_ena(mem_rd_ena),
-  .mem_wr_ena(mem_wr_ena),
+  mem_stage Mem_stage(
+    .clk(clock),
+    .rst(reset),
   
-  .exe_op1(exe_op1),
-  .exe_op2(exe_op2),
-  .is_word_opt(is_word_opt),
-  .op_info(op_info),
-  .alu_info(alu_info),
-  .bj_info(bj_info),
-  .load_info(load_info),
-  .save_info(save_info),
-
-  .reg_wr_ctrl(reg_wr_ctrl),
-  .csr_wr_ena(csr_wr_ena),
-  .csr_wr_addr(csr_wr_addr),
-  .rd_w_ena(rd_w_ena),
-  .rd_w_addr(rd_w_addr),
-
-  .jmp_imm(jmp_imm),
-
-  .id_excp(id_excp),
-  .excp_exit(excp_exit)
-);
-
-wire ex_allowin;
-wire ex_ready_go;
-wire ex_mem_valid;
-
-ex_stage Ex_stage(
-  .clk(clock),
-  .rst(reset),
-
-  .mem_allowin(mem_allowin),
-  .ex_allowin(ex_allowin),
-  .ex_ready_go(ex_ready_go),
-  .ex_mem_valid(ex_mem_valid),
-
-  .exe_op1(exe_op1),
-  .exe_op2(exe_op2),
-  .is_word_opt(is_word_opt),
-  .op_info(op_info),
-  .alu_info(alu_info),
-  .bj_info(bj_info),
-  .jmp_imm(jmp_imm),
-  .now_pc(pc),
-
-  .rd_data(exe_data),
-  .new_pc(new_pc),
-  .bj_ena(bj_ena)
-);
-
-wire mem_allowin;
-wire mem_ready_go;
-wire mem_wb_valid;
-
-mem_stage Mem_stage(
-  .clk(clock),
-  .rst(reset),
-
-  .wb_allowin(wb_allowin),
-  .mem_allowin(mem_allowin),
-  .mem_ready_go(mem_ready_go),
-  .mem_wb_valid(mem_wb_valid),
-
-  .r_data2(r_data2),
-  .load_info(load_info),
-  .save_info(save_info),
-  .mem_addr(mem_addr),
-  .mem_rd_data(mem_rd_data),
-
-  .mem_data(mem_data),
-  .mem_wr_data(mem_wr_data),
-  .byte_enable(byte_enable),
-  .mem_excp(mem_excp)
-);
-
-wire wb_allowin;
-
-wb_stage Wb_stage(
-  .clk(clock),
-  .rst(reset),
+    .wb_allowin(wb_allowin),
+    .mem_allowin(mem_allowin),
+    .mem_ready_go(mem_ready_go),
+    .mem_wb_valid(mem_wb_valid),
   
-  .wb_allowin(wb_allowin),
-
-  .reg_wr_ctrl(reg_wr_ctrl),
-  .exe_data(exe_data),
-  .mem_data(mem_data),
-  .pc_data(pc),
-  .csr_data(csr_rd_data),
-
-  .w_data(rd_data)
-);
-
-regfile Regfile(
-  .clk(clock),
-  .rst(reset),
-  .w_addr(rd_w_addr),
-  .w_data(rd_data),
-  .w_ena(rd_w_ena),
+    .r_data2(r_data2),
+    .load_info(load_info),
+    .save_info(save_info),
+    .mem_addr(mem_addr),
+    .mem_rd_data(mem_rd_data),
   
-  .r_addr1(rs1_r_addr),
-  .r_data1(r_data1),
-  .r_ena1(rs1_r_ena),
-  .r_addr2(rs2_r_addr),
-  .r_data2(r_data2),
-  .r_ena2(rs2_r_ena),
-
-  .regs_o(regs)
-);
-
-excp_handler Excp_handler(
-  .if_excp(if_excp),
-  .id_excp(id_excp),
-  .mem_excp(mem_excp),
-  .itrp_info(0),
-  .now_pc(pc),
-  .now_inst(inst),
-  .mem_acc_addr(0),
-  .excp_exit(excp_exit),
-
-  .excp_enter(excp_enter),
-  .mcause_wr_data(mcause_wr_data),
-  .mepc_wr_data(mepc_wr_data),
-  .mtval_wr_data(mtval_wr_data),
-  .mstatus_wr_data(mstatus_wr_data),
+    .mem_data(mem_data),
+    .mem_wr_data(mem_wr_data),
+    .byte_enable(byte_enable),
+    .mem_excp(mem_excp)
+  );
   
-  .mstatus_rd_data(mstatus_rd_data),
-  .mtvec_rd_data(mtvec_rd_data),
-  .mepc_rd_data(mepc_rd_data),
+  
+  // WB_STAGE
+  // pipeline control
+  wire wb_allowin;
+  // datapath
+  wire [`REG_BUS] exe_data;
+  wire [`REG_BUS] mem_data;
+  wb_stage Wb_stage(
+    .clk(clock),
+    .rst(reset),
+    
+    .wb_allowin(wb_allowin),
+    
+    .reg_wr_ctrl(reg_wr_ctrl),
+    .exe_data(exe_data),
+    .mem_data(mem_data),
+    .pc_data(pc),
+    .csr_data(csr_rd_data),
+    
+    .w_data(rd_data)
+  );
+  
+  // General Purpose Registers
+  // id_stage <-> regfile
+  wire rs1_r_ena;
+  wire [4 : 0]rs1_r_addr;
+  wire [`REG_BUS] r_data1;
+  wire rs2_r_ena;
+  wire [4 : 0]rs2_r_addr;
+  wire [`REG_BUS] r_data2;
+  // wb_stage <-> regfile
+  wire rd_w_ena;
+  wire [4 : 0]rd_w_addr;
+  wire [`REG_BUS] rd_data;
+  
+  // difftest
+  wire [`REG_BUS] regs[0 : 31];
 
-  .excp_jmp_ena(excp_jmp_ena),
-  .excp_pc(excp_pc)
-);
+  regfile Regfile(
+    .clk(clock),
+    .rst(reset),
+    .w_addr(rd_w_addr),
+    .w_data(rd_data),
+    .w_ena(rd_w_ena),
+  
+    .r_addr1(rs1_r_addr),
+    .r_data1(r_data1),
+    .r_ena1(rs1_r_ena),
+    .r_addr2(rs2_r_addr),
+    .r_data2(r_data2),
+    .r_ena2(rs2_r_ena),
+  
+    .regs_o(regs)
+  );
 
-csrfile CSRfile(
+  // exception
+  wire excp_enter, excp_exit;
+  
+  excp_handler Excp_handler(
+    .if_excp(if_excp),
+    .id_excp(id_excp),
+    .mem_excp(mem_excp),
+    .itrp_info(0),
+    .now_pc(pc),
+    .now_inst(inst),
+    .mem_acc_addr(0),
+    .excp_exit(excp_exit),
+
+    .excp_enter(excp_enter),
+    .mcause_wr_data(mcause_wr_data),
+    .mepc_wr_data(mepc_wr_data),
+    .mtval_wr_data(mtval_wr_data),
+    .mstatus_wr_data(mstatus_wr_data),
+  
+    .mstatus_rd_data(mstatus_rd_data),
+    .mtvec_rd_data(mtvec_rd_data),
+    .mepc_rd_data(mepc_rd_data),
+
+    .excp_jmp_ena(excp_jmp_ena),
+    .excp_pc(excp_pc)
+  );
+
+  // CSRs
+  // id_stage <-> csrfile
+  wire csr_rd_ena;
+  wire [11 : 0] csr_rd_addr;
+  wire csr_wr_ena;
+  wire [11 : 0] csr_wr_addr;
+  // csrfile <-> id_stage
+  wire [`REG_BUS] csr_rd_data;
+  // direct read and write
+  wire [`REG_BUS] mstatus_wr_data;
+  wire [`REG_BUS] mstatus_rd_data;
+  wire [`REG_BUS] mie_rd_data;
+  wire [`REG_BUS] mtvec_rd_data;
+  wire [`REG_BUS] mscratch_rd_data;
+  wire [`REG_BUS] mepc_wr_data;
+  wire [`REG_BUS] mepc_rd_data;
+  wire [`REG_BUS] mcause_wr_data;
+  wire [`REG_BUS] mcause_rd_data;
+  wire [`REG_BUS] mtval_rd_data;
+  wire [`REG_BUS] mtval_wr_data;
+  wire [`REG_BUS] mip_rd_data;
+
+  csrfile CSRfile(
   .clk(clock),
   .rst(reset),
 
