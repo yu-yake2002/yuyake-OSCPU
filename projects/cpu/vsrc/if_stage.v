da5bb6656279713486676d404f36f02e30fe2f46
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-04 09:07:29 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: f0ca167ba4f74bc29fe44c7f21382991     Virtualization: vmware   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  09:07:29 up 21:36,  1 user,  load average: 0.13, 0.10, 0.13

--- projects/cpu/vsrc/if_stage.v
+++ projects/cpu/vsrc/if_stage.v
@@ -103,7 +103,7 @@ module if_stage(
   
   always @(posedge clk) begin
     if (rst) begin
-      if_pc <= `PC_START - 64'h4;
+      if_pc <= `PC_START;
     end
     else if (pre_to_if_valid && if_allowin) begin
       if_pc <= next_pc;
