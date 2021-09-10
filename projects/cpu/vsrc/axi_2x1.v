7e1c38d33a15cf54b490dd606f956345fc2339cc
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-10 17:16:33 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: c65467950b8d4923b8117f5ae42a10a9     Virtualization: vmware   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  17:16:33 up  3:34,  1 user,  load average: 1.60, 0.85, 0.45

--- projects/cpu/vsrc/axi_2x1.v
+++ projects/cpu/vsrc/axi_2x1.v
@@ -425,7 +425,7 @@ module axi_2x1 # (
   assign r_id_o_0      = {AXI_ID_WIDTH{r_state_0}}    & top_r_id_i;
   assign r_user_o_0    = {AXI_USER_WIDTH{r_state_0}}  & top_r_user_i;
   
-  wire [63 : 0] test_wire = {AXI_DATA_WIDTH{r_state_0}}  & top_r_data_i;
+  wire [63 : 0] test_wire1 = {AXI_DATA_WIDTH{r_state_0}}  & top_r_data_i;
 
   // to IF
   assign ar_ready_o_1  = r_state_1                    & top_ar_ready_i;
@@ -435,6 +435,8 @@ module axi_2x1 # (
   assign r_last_o_1    = r_state_1                    & top_r_last_i;
   assign r_id_o_1      = {AXI_ID_WIDTH{r_state_1}}    & top_r_id_i;
   assign r_user_o_1    = {AXI_USER_WIDTH{r_state_1}}  & top_r_user_i;
+
+  wire [63 : 0] test_wire2 = {AXI_DATA_WIDTH{r_state_1}}  & top_r_data_i;
 
   // to RAM
   assign top_ar_valid_o = (
