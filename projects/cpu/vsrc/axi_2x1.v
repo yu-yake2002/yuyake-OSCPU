f8efbdaaa88ab4ae893761fd2ec7c832eaf8e186
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-10 13:58:18 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: c65467950b8d4923b8117f5ae42a10a9     Virtualization: vmware   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:58:18 up 15 min,  1 user,  load average: 0.63, 0.45, 0.43

--- projects/cpu/vsrc/axi_2x1.v
+++ projects/cpu/vsrc/axi_2x1.v
@@ -420,7 +420,8 @@ module axi_2x1 # (
   assign ar_ready_o_0  = r_state_0                    & top_ar_ready_i;
   assign r_valid_o_0   = r_state_0                    & top_r_valid_i;
   assign r_resp_o_0    = {2{r_state_0}}               & top_r_resp_i;
-  assign r_data_o_0    = {AXI_DATA_WIDTH{r_state_0}}  & top_r_data_i;
+  //assign r_data_o_0    = {AXI_DATA_WIDTH{r_state_0}}  & top_r_data_i;
+  assign r_data_o_0    = top_r_data_i;
   assign r_last_o_0    = r_state_0                    & top_r_last_i;
   assign r_id_o_0      = {AXI_ID_WIDTH{r_state_0}}    & top_r_id_i;
   assign r_user_o_0    = {AXI_USER_WIDTH{r_state_0}}  & top_r_user_i;
@@ -429,7 +430,8 @@ module axi_2x1 # (
   assign ar_ready_o_1  = r_state_1                    & top_ar_ready_i;
   assign r_valid_o_1   = r_state_1                    & top_r_valid_i;
   assign r_resp_o_1    = {2{r_state_1}}               & top_r_resp_i;
-  assign r_data_o_1    = {AXI_DATA_WIDTH{r_state_1}}  & top_r_data_i;
+  //assign r_data_o_1    = {AXI_DATA_WIDTH{r_state_1}}  & top_r_data_i;
+  assign r_data_o_1    = top_r_data_i;
   assign r_last_o_1    = r_state_1                    & top_r_last_i;
   assign r_id_o_1      = {AXI_ID_WIDTH{r_state_1}}    & top_r_id_i;
   assign r_user_o_1    = {AXI_USER_WIDTH{r_state_1}}  & top_r_user_i;
