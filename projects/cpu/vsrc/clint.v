a9c9f152579fb4f1456067edf9d121322946709f
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-26 18:03:29 +0800

    孙际儒 20210611    Static hostname: yuyake-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: b971ed0d9c004e3d8fec2455825bf7a0            Boot ID: 3f20c48b4c8f4f1e9a173f35f6b5eafc     Virtualization: vmware   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  18:03:29 up  1:42,  1 user,  load average: 0.68, 0.61, 0.38

--- projects/cpu/vsrc/clint.v
+++ projects/cpu/vsrc/clint.v
@@ -221,7 +221,7 @@ module clint # (
   end
   
   // decelerate
-  reg [4:0] decelerate_reg;
+  reg [3:0] decelerate_reg;
   always @(posedge clk) begin
     if (rst) begin
       decelerate_reg <= 0;
