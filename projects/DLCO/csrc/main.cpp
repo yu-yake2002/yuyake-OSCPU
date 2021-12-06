6f3d51c4d4f3ef1b3f26c097feee53a1b3c59ea2
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-06 20:36:04 +0800

    孙际儒 20210611    Static hostname: yuyake-ubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 361d6b645afa4317a8e3e2078b6c2a9b            Boot ID: 4666ebb3069f4bcb9ea2c15139decc92   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:36:04 up  1:45,  1 user,  load average: 0.77, 0.89, 0.83

--- projects/DLCO/csrc/main.cpp
+++ projects/DLCO/csrc/main.cpp
@@ -1,20 +1,24 @@
 #include "Vtop.h"
 #include <memory>
 #include <iostream>
-#include <verilated.h>
 #include <stdint.h>
 #include <assert.h>
 
 static Vtop* dut_ptr;
 
+#ifdef VM_TRACE         // --trace
+#include <verilated_vcd_c.h>
+static VerilatedVcdC* fp;      //to form *.vcd file
+#endif
 
 //mycpu运行一个周期
-void next_cycle(){
-  dut_ptr->eval();
+void next_cycle(int time){
   dut_ptr->clk = 0;
   dut_ptr->eval();
+  fp->dump(time * 2 + 1);
   dut_ptr->clk = 1;
   dut_ptr->eval();
+  fp->dump(time * 2 + 2);
 }
 
 //将输入的Test的字符串读取为Int64传入硬件，使其初始化读取Mem。
@@ -40,12 +44,12 @@ int main(int argc, char **argv) {
   // initial， reset的时候imem读取对应的testbench。
   dut_ptr->reset = 1;
   dut_ptr->testcase = testcase;
-  next_cycle();
-  next_cycle();
+  next_cycle(0);
+  next_cycle(1);
   dut_ptr->reset = 0;
 
-  for (int i = 0; i < 1000; i++) {
-    next_cycle();
+  for (int i = 2; i < 1002; i++) {
+    next_cycle(i);
     if(dut_ptr->done == 1){
       printf("Finish Test\n");
       break;
